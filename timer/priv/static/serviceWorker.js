e12b3e72fc715745561a74d2e4c593eaa765ac0f
Axel <axel.baudot@protonmail.com>
2025-04-23 12:32:54 -0400

    tiimer: fix service worker for caching

--- timer/priv/static/serviceWorker.js
+++ timer/priv/static/serviceWorker.js
@@ -1,12 +1,25 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/serviceWorker.js")
-      .then((_) => {
-        console.log("Service Worker registered");
-      })
-      .catch((registrationError) => {
-        console.error("Service Worker registration failed:", registrationError);
-      });
-  });
-}
+const CACHE_NAME = "timer-cache-v1";
+
+// List of files to cache
+const urlsToCache = [
+  "./index.html",
+  "./priv/static/tiime.css", // Your CSS file
+  "./priv/static/tiime.js", // Your JavaScript file
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    }),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }),
+  );
+});
